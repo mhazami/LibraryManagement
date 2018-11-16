b5d65e5f35f40f371582d6db99615575a28f51c8
Mohammad Azami <mohammadazami98@yahoo.com>
2018-11-16 03:40:44 +0330

    Create_BusinessBase_In_DAL

---
+++ BLL/BookBO.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.BLL.Base;
+using LibraryManagement.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.BLL
+{
+    public class BookBO : BusinessBase<Book>
+    {
+       
+    }
+}
