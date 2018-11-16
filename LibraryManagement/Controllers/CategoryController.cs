bef15c8b52df3d2ac0c79d65f545e9a5e601405a
Mohammad Azami <mohammadazami98@yahoo.com>
2018-11-16 04:23:29 +0330

    fixed

---
+++ LibraryManagement/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagement.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
